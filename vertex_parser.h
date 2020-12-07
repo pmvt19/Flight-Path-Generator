c3eb35578e1d51a774fc8e7dc64465f89581bf18
ptelag2 <ptelag2@illinois.edu>
2020-12-07 13:19:24 -0600

    Added comments to vertex parser

--- vertex_parser.h
+++ vertex_parser.h
@@ -17,6 +17,14 @@ class VertexParser {
         ifstream file;
 
     public:
+        /**
+         * @param file_path - File path of the file to open
+         * @return - True if the file was successfully opened; False otherwise
+         */
         bool OpenFile(const string& file_path);
+
+        /**
+         * @return - A Vector of all the vertices to be added to a graph
+         */
         vector<Airport*> GenerateVertices();
 };
